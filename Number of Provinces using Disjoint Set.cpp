638a7af1b900643cada5b221f1eeddfe3bde5f46
Param Sahai <74666867+param-sahai@users.noreply.github.com>
2023-06-21 03:51:48 +0530

    Create Number of Provinces using Disjoint Set.cpp

---
+++ Number of Provinces using Disjoint Set.cpp
@@ -0,0 +1,55 @@
+class DisjointSet{
+  public:
+  vector<int>parent, size;
+   DisjointSet(int n){
+       parent.resize(n+1);
+       size.resize(n+1, 1);
+       for(int i=0;i<=n;i++){
+           parent[i] = i;
+       }
+   }
+   int findParent(int n){
+       if(parent[n]==n) return n;
+       else
+       return parent[n] = findParent(parent[n]);
+   }
+   
+   void UnionBySize(int u, int v){
+       int parent_u = findParent(u);
+       int parent_v = findParent(v);
+       
+       if(parent_u == parent_v) return;
+       if(size[parent_v] > size[parent_u])
+        {
+            parent[parent_u] = parent_v;
+            size[parent_v]+=size[parent_u];
+        }
+       else
+        {
+            parent[parent_v] = parent_u;
+            size[parent_u]+=size[parent_v];
+        }
+       
+   }
+};
+
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        DisjointSet ds(V);
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(isConnected[i][j]==1)
+                    ds.UnionBySize(i, j);
+            }
+        }
+        int count = 0;
+        for(int i=0;i<V;i++){
+            if(ds.parent[i]==i)
+                count++;
+        }
+        return count;
+    }
+};
